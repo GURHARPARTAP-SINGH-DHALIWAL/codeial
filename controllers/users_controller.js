1ab42a203f8212322cb2f8bb8ca4bdca299e6de7
Gurharpartap Singh <gurharpartapdhaliwal@gmail.com>
2021-05-11 03:37:16 +0530

    Add Sign In

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,9 +1,26 @@
 //USer module is required
 const User=require('../models/users');
 module.exports.profile=function(req,res){
-    return res.render('users',{
-        name:"GSD"
-    });
+   User.findById(req.cookies.user_id,function(err,user){
+       //if(err){console.log('Error in Loading Profile Page');return ;}
+       if(!(req.cookies.user_id))
+       {
+           return res.redirect('user/sign-in');
+       }
+   
+       if(user)
+       {
+           console.log("hello");
+        return res.render('users',{
+            title:"Profile",
+            name:user.name,
+            email:user.email
+        });
+       }
+       else
+       return res.redirect('/user/sign-in');
+   });
+   
 };
 module.exports.signUp=function(req,res){
     return res.render('user_sign_up',{
@@ -37,6 +54,32 @@ module.exports.create=function(req,res){
                 if(err){console.log('Error in Creating User in DB');return ;}
                 return res.redirect('/user/sign-in');
             });
+        }
+    });
+};
+module.exports.createSession=function(req,res){
+    //Find User
+    User.findOne({email:req.body.email},function(err,user){
+        if(err){console.log('Error in Signing In the user');return ;}
+        if(user)
+        {   
+            //Found
+            console.log(req.body);
+            console.log(user);
+             //var userObj=user.toObject();
+            if(user.password!=req.body.password)
+            { //Wrong Password
+                return res.redirect('back');
+            }
+            else
+            {   
+                res.cookie('user_id',user.id);
+                return res.redirect('/user/profile');
+            }
+        }
+        else
+        {
+            return res.redirect('back');
         }
     });
 };
