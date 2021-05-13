d9db79f9f9d2e71bbfe4791c8b40cf37554c659a
Gurharpartap Singh <gurharpartapdhaliwal@gmail.com>
2021-05-13 16:39:53 +0530

    Display Posts On Home Page with USername

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -4,8 +4,19 @@ module.exports.home=function(req,res){
     console.log(req.cookies);
     // Post.find({},function(err,post){
     //     if(err){console.log('Error in Displaying Posts');return;}
+    //     return res.render('home',{
+    //         title:"Home Page",
+    //         posts:post
+    //     });
     // });
-    return res.render('home',{
-        title:"Home Page"
+    Post.find({}).populate('user').exec(function(err,post){
+        if(err){console.log('Error in Displaying Posts');return;}
+        return res.render('home',{
+            title:"Home Page",
+            posts:post
+        });
     });
+    // return res.render('home',{
+    //     title:"Home Page"
+    // });
 };
