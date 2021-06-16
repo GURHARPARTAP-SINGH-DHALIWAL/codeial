47aa0a600a66e63d4693f394961442ad7380f45f
Gurharpartap Singh <gurharpartapdhaliwal@gmail.com>
2021-06-16 15:29:05 +0530

    Async Await in Post Cintroller

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -11,20 +11,29 @@ module.exports.create=function(req,res){
 
 }
 
-module.exports.destroy=function(req,res){
-    Post.findById(req.params.id,function(err,post){
-        if(err){console.log('Error in Deleting Post');return ;}
-        if(post&&post.user==req.user.id)
-        {
-            post.remove();
-            Comment.deleteMany({post:req.params.id},function(err){
-                if(err){console.log('Error in Deleting Comments While Deleting a Post');return ;}
-                return res.redirect('back');
-            });
-        }
-        else
-        {
-            return res.redirect('back');
-        }
-    });
+module.exports.destroy=async function(req,res){
+    let post=await Post.findById(req.params.id);
+    if(post&&post.user==req.user.id)
+    {
+    await post.remove();
+    await Comment.deleteMany({post:post._id}); 
+    }
+    return res.redirect('back');
+    
+    
+    // Post.findById(req.params.id,function(err,post){
+    //     if(err){console.log('Error in Deleting Post');return ;}
+    //     if(post&&post.user==req.user.id)
+    //     {
+    //         post.remove();
+    //         Comment.deleteMany({post:req.params.id},function(err){
+    //             if(err){console.log('Error in Deleting Comments While Deleting a Post');return ;}
+    //             return res.redirect('back');
+    //         });
+    //     }
+    //     else
+    //     {
+    //         return res.redirect('back');
+    //     }
+    // });
 };
