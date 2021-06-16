c524e419ca2008cd0330beb3d6ed9ef5d00fe2d4
Gurharpartap Singh <gurharpartapdhaliwal@gmail.com>
2021-06-16 14:30:34 +0530

    First Async Await

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -2,39 +2,53 @@ const Post=require('../models/posts');
 const Comment=require('../models/comment');
 const User=require('../models/users');
 
-module.exports.home=function(req,res){
-    console.log(req.cookies);
-    // Post.find({},function(err,post){
-    //     if(err){console.log('Error in Displaying Posts');return;}
-    //     return res.render('home',{
-    //         title:"Home Page",
-    //         posts:post
-    //     });
-    // });
-    //populate the 'user' field with object
-    Post.find({}).populate('user')
-    .populate({
+module.exports.home=async function(req,res){
+
+    let post=await Post.find({}).populate('user').populate({
         path:'comments',
         populate:{
             path:'user'
         }
-    })
-    .exec(function(err,post){
-        if(err){console.log('Error in Displaying Posts');return;}
-       
-        User.find({},function(err,users){
-            if(err){console.log('Error in Dislplaying Friends List');return;}
-
-            return res.render('home',{
-                title:"Home Page",
-                posts:post,
-                all_users:users
-                
-            });
-        });
-       
     });
-    // return res.render('home',{
-    //     title:"Home Page"
+    let users=await User.find({});
+    return res.render('home',{
+        title:'Home Page',
+        posts:post,
+        all_users:users
+
+    });
+    // console.log(req.cookies);
+    // // Post.find({},function(err,post){
+    // //     if(err){console.log('Error in Displaying Posts');return;}
+    // //     return res.render('home',{
+    // //         title:"Home Page",
+    // //         posts:post
+    // //     });
+    // // });
+    // //populate the 'user' field with object
+    // Post.find({}).populate('user')
+    // .populate({
+    //     path:'comments',
+    //     populate:{
+    //         path:'user'
+    //     }
+    // })
+    // .exec(function(err,post){
+    //     if(err){console.log('Error in Displaying Posts');return;}
+       
+    //     User.find({},function(err,users){
+    //         if(err){console.log('Error in Dislplaying Friends List');return;}
+
+    //         return res.render('home',{
+    //             title:"Home Page",
+    //             posts:post,
+    //             all_users:users
+                
+    //         });
+    //     });
+       
     // });
+    // // return res.render('home',{
+    // //     title:"Home Page"
+    // // });
 };
